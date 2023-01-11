c42f28d67edf6a770a37d5756c2a4e5f16eab1bd
Rezwanul Islam <rezwanul7@gmail.com>
2023-01-11 22:49:20 +0600

    update main title

--- src/main/java/com/rezwanul7/Main.java
+++ src/main/java/com/rezwanul7/Main.java
@@ -2,6 +2,6 @@ package com.rezwanul7;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Java Programming");
+        System.out.println("Poor Man's Java Programming");
     }
 }
